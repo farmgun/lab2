8f6215d684104082854f587fead2f5f2d24944b8
farmgun <65977020+farmgun@users.noreply.github.com>
2020-06-08 00:23:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,120 +1,134 @@
-﻿using System;
+﻿
+using System;
 
-namespace figurs
+
+namespace cat
 {
-    abstract class figur
+    
+    public class cat
     {
-        public abstract double Perimeter();
-        public abstract double Area();
+        public enum Gender 
+        {
+            female,
+            male
+        };
+        
+        string name;
+        Gender gender;
+        double Energy;
+        public static readonly double MaxEnergy = 20;
+        public static readonly double MinEnergy = 0;
+        public static readonly double SleepEnergyGain = 10;
+        public static readonly double JumpEnergyDrain = 0.5;
+
+
+        public cat(string name, Gender gender)
+        {
+            
+            this.name = name;
+            this.gender = gender;
+            Energy = MaxEnergy;
+        }
+        ~cat()
+        {
+
+        }
+
+        public void Output ()
+        {
+            Console.WriteLine(this.name + " " + this.gender + " Energy = " + this.Energy);
+        }
+
+        public void Jump()
+        {
+            if (this.Energy >= 0.5)
+            { 
+                this.Energy -= JumpEnergyDrain;
+                Console.WriteLine(this.Energy);
+            }
+            else
+            {
+                this.Energy = 0;
+                Console.WriteLine("Not enough energy to jump!");
+            }
+
+        }
+
+        public void Sleep()
+        {
+            if (this.Energy <= 10)
+            { 
+                this.Energy += SleepEnergyGain;
+                Console.WriteLine(this.Energy);
+            }
+            else
+            {
+                this.Energy = 20;
+                Console.WriteLine("Energy restored!");
+            }
+        }
 
     }
-
-    class Rectangle : figur
+    class Program
     {
-        public double Width { get; set; }
-        public double Height { get; set; }
-
-        public Rectangle(double width, double height)
+        static void Main(string[] args)
         {
-            this.Width = width;
-            this.Height = height;
+            
+            cat c1 = new cat("barsik",cat.Gender.female);
+            c1.Output();
+            c1.Sleep();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Output();
+            c1.Sleep();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump(); 
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump(); 
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump(); 
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
         }
-        public override double Perimeter()
-        {
-            return Width * 2 + Height * 2;
-        }
-        public override double Area()
-        {
-            return Width * Height;
-        }
-    }
-
-    class Circle : figur
-    {
-        public float Radius { get; set; }
-        public Circle(float radius)
-        {
-            this.Radius = radius;
-        }
-        public override double Perimeter()
-        {
-            return Radius * 2 * Math.PI;
-        }
-        public override double Area()
-        {
-            return Radius * Radius * Math.PI;
-        }
-
-        class Triangle : figur
-        {
-            public double First { get; set; }
-            public double Second { get; set; }
-            public double Third { get; set; }
-
-
-
-            public Triangle(double first, double second, double third)
-            {
-                this.First = first;
-                this.Second = second;
-                this.Third = third;
-            }
-
-
-            public override double Perimeter()
-            {
-                return First + Second + Third;
-            }
-            public override double Area()
-            {
-                double p = (First + Second + Third) / 2;
-                double S = Math.Sqrt(p * (p - First) * (p - Second) * (p - Third));
-                return S;
-            }
-        }
-
-        public abstract class Animal
-        {
-
-            public Animal(float Weight, string Color)
-            {
-                this.Weight = Weight;
-                this.Color = Color;
-            }
-
-            public float Weight
-            {
-                get; set;
-            }
-
-            public string Color
-            {
-                get; set;
-            }
-
-            public abstract string MakeSound();
-            static void Main(string[] args)
-            {
-
-            }
-
-        }
-
-        class Cat : Animal
-        {
-            string Sound;
-            public Cat(float weight, string color) : base(100, "rainbow")
-            {
-                this.Weight = weight;
-                this.Color = color;
-                Sound = MakeSound();
-            }
-
-            public override string MakeSound()
-            {
-                return "Myau";
-            }
-        }
-
     }
 }
